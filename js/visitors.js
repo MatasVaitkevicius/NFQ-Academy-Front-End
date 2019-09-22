b2e321a46908a58c7745a503de76580792e38d54
Matas Vaitkevičius <vaitkevicius.matas@gmail.com>
2019-09-22 15:24:44 +0300

    Fetch visitors from local storage

--- js/visitors.js
+++ js/visitors.js
@@ -1,17 +1,3 @@
-import VisitorsList from './../visitorsList.js'
-
-const fetchVisitors = () => {
-  document.getElementById('visitors_list').innerHTML =
-    `<ul>
-    ${VisitorsList.map(v => `<li>${v.visitorId}
-    | ${v.visitorName}
-    | ${v.visitorSurname}
-    | ${v.requiredSpecialist}
-    | ${v.timeRegistered}</li>`).join("")}
-  </ul>
-`;
-}
-// document.getElementById('add_visitor_btn')
-//   .addEventListener('click', addNewVisitorClick.bind(), false)
+const fetchVisitors = () => JSON.parse(window.localStorage.getItem('visitorListLocalStorage')) || [];
 
 export default fetchVisitors
