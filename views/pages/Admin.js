11317f32ace429fa57d9b3abf5266a756a158672
Matas Vaitkevičius <vaitkevicius.matas@gmail.com>
2019-09-22 16:19:53 +0300

    Added visitor list to Admin page

--- views/pages/Admin.js
+++ views/pages/Admin.js
@@ -1,16 +1,19 @@
 import addVisitorForm from './../../js/admin.js';
+import specialistPage from './../../js/specialist.js';
 
 const Admin = {
   render: async () => {
     const view = /*html*/
       `
     <div id="add_visitor"></div>
+    <div id="saved-visitor-list"></div>
     <h1>Hello admin</h1>
     `
     return view
   },
   after_render: async () => {
     addVisitorForm()
+    specialistPage()
   }
 }
 
